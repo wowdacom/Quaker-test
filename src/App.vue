455fdfa47110b9541f9bd9b3d2897cc3d5869ff7
Jim Wu Hu <wowdacom@gmail.com>
2018-09-25 09:17:19 +0800

    第一版

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
     <Main/>
-    <div class="introduction">
-      <div class="introduction-content">
-        Choosing where you want to live when you retire is often seen as a big decision — but it’s also an exciting one. After all, this next phase is one where you’ll have more time to devote to your favorite pursuits, whether that’s traveling, volunteering, spending more time with loved ones or being even more active in your community.
-The big question, though, is how do you find a place that meets your core interests? Your ideal lifestyle for retirement might be anchored in the great outdoors; include access to the best of art, food and culture; or be designed to keep your cost of living modest. Since those three qualities loom large for many people, we conducted a unique study to identify and rank the top 10 cities for those three distinct priorities, represented in the maps below.
-The final destination that you’ll call home? We’ll leave that to you.
-      </div>
-        <div class="map-wrapper">
-      </div>
-    </div>
   </div>
 </template>
 
@@ -31,23 +22,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   .introduction {
-  border: solid 1px black;
-
-  height: 500px;
-  .introduction-content {
-    position: absolute;
-    top: 35%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 80%;
-    line-height: 100px;
-    }
+    border: solid 1px black;
   }
   .map-wrapper {
     width: 100%;
-    height: 500px;
     background-image: url("~/static/world-map");
   }
 }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="main">
-    <h1>Hello I am Your Main</h1>
+    <div class="space">
+      I am space
+    </div>
+    <h1 :style="steps[step]">Hello I am Your Main</h1>
+    <img class="start-map" :src="map.img" alt="">
+    <div class="hints-wrapper">
+      <div class="hint-box1">hint1</div>
+      <div class="hint-box2">hint2</div>
+      <div class="hint-box3">hint3</div>
+      <div class="hint-box4">hint4</div>
+      <div class="hint-box5">hint5</div>
+      <img class="end-map" :src="map.img" alt="">
+    </div>
+    <img class="map-fixed" :src="map.img" :style="steps[step]" alt="">
+    
+    <p>1234</p>
   </div>
 </template>
 
@@ -9,13 +24,252 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      map: {
+        img: require('@/assets/map.jpg'),
+        offsetStart: 0,
+        offsetEnd: 0
+      },
+      step: 0,
+      screenHeight: 0,
+      steps: [
+        {
+          position: 'fixed',
+          top: '0',
+          left: '0',
+          opacity: '0',
+          transform: 'scale(1)',
+          'transform-origin': '0 0'
+        },
+        {
+          position: 'fixed',
+          top: '0',
+          left: '0',
+          opacity: '1',
+          transform: 'scale(2)',
+          'transform-origin': '0 0'
+        }
+        // {
+        //   position: 'fixed',
+        //   top: '0',
+        //   left: '0',
+        //   transform: 'scale(2)',
+        //   opacity: '1',
+        //   'transform-origin': '50% 50%'
+        // },
+        // {
+        //   position: 'fixed',
+        //   top: '0',
+        //   left: '0',
+        //   transform: 'scale(2)',
+        //   opacity: '1',
+        //   'transform-origin': '100% 0'
+        // },
+        // {
+        //   position: 'fixed',
+        //   top: '0',
+        //   left: '0',
+        //   transform: 'scale(2)',
+        //   opacity: '1',
+        //   'transform-origin': '100% 100%'
+        // },
+        // {
+        //   position: 'fixed',
+        //   top: '0',
+        //   left: '0',
+        //   transform: 'scale(1)',
+        //   opacity: '1',
+        //   'transform-origin': '0 100%'
+        // },
+        // {
+        //   position: 'fixed',
+        //   top: '0',
+        //   left: '0',
+        //   transform: 'scale(1)',
+        //   opacity: '1',
+        //   'transform-origin': '0 0'
+        // }
+      ]
     }
+  }, 
+  methods: {
+    handleScroll () {
+      let currentHieght = window.pageYOffset
+
+      if ( this.map.offsetStart < currentHieght && currentHieght < this.map.offsetEnd ) {
+        this.step = 1;
+      } else {
+        this.step = 0;
+      }
+      // if (this.map.offset < currentHieght && currentHieght < this.map.offset + 1000 ) {
+      //   this.step = 1;
+      // } else if ( this.map.offset + 1001 < currentHieght && currentHieght < this.map.offset + 2000 ) {
+      //   this.step = 2;
+      // } else if ( this.map.offset + 2001 < currentHieght && currentHieght < this.map.offset + 3000 ) {
+      //   this.step = 3;
+      // } else if ( this.map.offset + 3001 < currentHieght && currentHieght < this.map.offset + 4000 ) {
+      //   this.step = 4;
+      // } else if ( this.map.offset + 4001 < currentHieght && currentHieght < this.map.offset + 5000 ) {
+      //   this.step = 5;
+      // } else {
+      //   this.step = 0;
+      // }
+    }
+  },
+  mounted () {
+    // let bodyRect   = document.body.getBoundingClientRect(),
+    //     mapStartElement = document.querySelector('.page-start'),  
+    //     mapStartRect   = mapStartElement.getBoundingClientRect(),
+    //     mapEndElement = document.querySelector('.page-end'),  
+    //     mapEndRect   = mapEndElement.getBoundingClientRect(),
+    //     offsetStart     = mapStartRect.top - bodyRect.top,
+    //     offsetEnd       = mapEndRect.top - bodyRect.top ;
+
+    //     this.map.start = offsetStart;
+    //     this.map.end = offsetEnd;
+
+
+    //     console.log(offsetStart);
+    //     console.log(offsetEnd);
+    let bodyRect   = document.body.getBoundingClientRect(),
+        mapStartElement = document.querySelector('.start-map'),  
+        mapStartRect  = mapStartElement.getBoundingClientRect(),
+        offsetStart   = mapStartRect.top - bodyRect.top,
+        mapEndElement = document.querySelector('.end-map'),  
+        mapEndRect    = mapEndElement.getBoundingClientRect(),
+        offsetEnd     = mapEndRect.top - bodyRect.top
+        // wrapper       = document.querySelector('.hints-wrapper')
+        // wrapper.style.height = document.body.clientHeight * 5
+
+        this.screenHeight = document.body.clientHeight
+        this.map.offsetStart = offsetStart
+        this.map.offsetEnd = offsetEnd
+
+        console.log(this.screenHeight)
+        console.log(this.map.offsetStart)
+        console.log(this.map.offsetEnd)
+
+        window.addEventListener('scroll', this.handleScroll)
+
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+.main {
+  height: 100000px;
+}
+.space {
+  height: 3000px;
+}
+h1 {
+  cursor: pointer;
+}
+.start-map {
+  position: static;
+}
+.hints-wrapper {
+  @media only screen and (max-width: 600px) {
+    width: 100vw;
+    right: 0;
+    height: 6000px;
+  }
+  position: relative;
+  height: 8000px;
+}
+.hints-wrapper > .hint-box1 {
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+    right: 10;
+  }
+  position: absolute;
+  z-index: 2;
+  right: 100px;
+  top: 0%;
+  height: 200px;
+  width: 400px;
+  border: solid 1px	#00BFFF;
+  border-radius: 5px;
+  background-color: rgba($color: white, $alpha: 0.2)
+}
+.hints-wrapper > .hint-box2 {
+  @media only screen and (max-width: 600px) {
+    width: 100vw;
+    right: 0;
+  }
+  position: absolute;
+  z-index: 2;
+  right: 100px;
+  top: 20%;
+  height: 200px;
+  width: 400px;
+  border: solid 1px	#00BFFF;
+  border-radius: 5px;
+  background-color: rgba($color: white, $alpha: 0.2)
+}
+.hints-wrapper > .hint-box3 {
+  @media only screen and (max-width: 600px) {
+    width: 100vw;
+    right: 0;
+  }
+  position: absolute;
+  z-index: 2;
+  right: 100px;
+  top: 40%;
+  height: 200px;
+  width: 400px;
+  border: solid 1px	#00BFFF;
+  border-radius: 5px;
+  background-color: rgba($color: white, $alpha: 0.2)
+}
+.hints-wrapper > .hint-box4 {
+  @media only screen and (max-width: 600px) {
+    width: 100vw;
+    right: 0;
+  }
+  position: absolute;
+  z-index: 2;
+  right: 100px;
+  top: 60%;
+  height: 200px;
+  width: 400px;
+  border: solid 1px	#00BFFF;
+  border-radius: 5px;
+  background-color: rgba($color: white, $alpha: 0.2)
+}
+.hints-wrapper > .hint-box5 {
+  @media only screen and (max-width: 600px) {
+    width: 100vw;
+    right: 0;
+  }
+  position: absolute;
+  z-index: 2;
+  right: 100px;
+  top: 80%;
+  height: 200px;
+  width: 400px;
+  border: solid 1px	#00BFFF;
+  border-radius: 5px;
+  background-color: rgba($color: white, $alpha: 0.2)
+}
+.end-map {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+.map-fixed {
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  opacity: 1;
+  transform: scale(1);
+  transform-origin: 0 0;
+  height: auto;
+  transition: all 0.5s;
+}
 
 </style>
