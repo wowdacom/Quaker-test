623cf631e3600808fe82e190a3c2132066a93c81
unknown <06823@udnprod.local>
2018-09-25 16:43:19 +0800

    change way to move map

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,19 +3,16 @@
     <div class="space">
       I am space
     </div>
-    <h1 :style="steps[step]">Hello I am Your Main</h1>
-    <img class="start-map" :src="map.img" alt="">
-    <div class="hints-wrapper">
-      <div class="hint-box1">hint1</div>
-      <div class="hint-box2">hint2</div>
-      <div class="hint-box3">hint3</div>
-      <div class="hint-box4">hint4</div>
-      <div class="hint-box5">hint5</div>
-      <img class="end-map" :src="map.img" alt="">
+      <h1>Start of The map</h1>
+    <div class="hints-wrapper" :style="mapAnimation.steps[mapAnimation.step]">
+      <!-- <div v-for="(item, index) in map.hints" :key="index" class="hint-box">
+        <h5>{{ item.title }}</h5>
+        {{ item.content }}
+      </div> -->
+      <!-- <img class="start-map" :src="map.img" alt=""> -->
     </div>
-    <img class="map-fixed" :src="map.img" :style="steps[step]" alt="">
     
-    <p>1234</p>
+    <p>End of the Map</p>
   </div>
 </template>
 
@@ -24,130 +21,116 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      pageInfo: {
+        screenHeight: 0
+      },
       map: {
         img: require('@/assets/map.jpg'),
         offsetStart: 0,
-        offsetEnd: 0
+        offsetEnd: 0,
+        height: 0,
+        hints: [
+          {
+            title: 'hint 1',
+            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, nihil.'
+          },
+          {
+            title: 'hint 2',
+            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, nihil.'
+          },
+          {
+            title: 'hint 3',
+            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, nihil.'
+          },
+          {
+            title: 'hint 4',
+            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, nihil.'
+          },
+          {
+            title: 'hint 5',
+            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, nihil.'
+          }
+        ]
       },
-      step: 0,
-      screenHeight: 0,
-      steps: [
-        {
-          position: 'fixed',
-          top: '0',
-          left: '0',
-          opacity: '0',
-          transform: 'scale(1)',
-          'transform-origin': '0 0'
-        },
-        {
-          position: 'fixed',
-          top: '0',
-          left: '0',
-          opacity: '1',
-          transform: 'scale(2)',
-          'transform-origin': '0 0'
-        }
-        // {
-        //   position: 'fixed',
-        //   top: '0',
-        //   left: '0',
-        //   transform: 'scale(2)',
-        //   opacity: '1',
-        //   'transform-origin': '50% 50%'
-        // },
-        // {
-        //   position: 'fixed',
-        //   top: '0',
-        //   left: '0',
-        //   transform: 'scale(2)',
-        //   opacity: '1',
-        //   'transform-origin': '100% 0'
-        // },
-        // {
-        //   position: 'fixed',
-        //   top: '0',
-        //   left: '0',
-        //   transform: 'scale(2)',
-        //   opacity: '1',
-        //   'transform-origin': '100% 100%'
-        // },
-        // {
-        //   position: 'fixed',
-        //   top: '0',
-        //   left: '0',
-        //   transform: 'scale(1)',
-        //   opacity: '1',
-        //   'transform-origin': '0 100%'
-        // },
-        // {
-        //   position: 'fixed',
-        //   top: '0',
-        //   left: '0',
-        //   transform: 'scale(1)',
-        //   opacity: '1',
-        //   'transform-origin': '0 0'
-        // }
-      ]
+      mapAnimation: {
+        step: 0,
+        steps: [
+          {
+            'background-attachment': 'scroll',
+            'background-size': '100%'
+          },
+          {
+            'background-attachment': 'fixed',
+            'background-size': '100%',
+            'background-position': '50% 50%'
+          },
+          {
+            'background-attachment': 'fixed',
+            'background-size': '400%',
+            'background-position': '0 0'
+          },
+          {
+            'background-attachment': 'fixed',
+            'background-size': '400%',
+            'background-position': '0 100%'
+          },
+          {
+            'background-attachment': 'fixed',
+            'background-size': '400%',
+            'background-position': '100% 0'
+          },
+          {
+            'background-attachment': 'fixed',
+            'background-size': '400%',
+            'background-position': '100% 100%'
+          },
+          {
+            'background-attachment': 'fixed',
+            'background-size': '400%',
+            'background-position': '50% 50%'
+          },
+          {
+            'background-attachment': 'scroll',
+            'background-size': '100%'
+          }
+        ]
+      }
     }
   }, 
   methods: {
     handleScroll () {
-      let currentHieght = window.pageYOffset
-
-      if ( this.map.offsetStart < currentHieght && currentHieght < this.map.offsetEnd ) {
-        this.step = 1;
+      let currentHieght = window.pageYOffset,
+          wrapperSection = this.map.height/6
+          console.log(wrapperSection)
+      if ( this.map.offsetStart < currentHieght && currentHieght < this.map.offsetStart + wrapperSection ) {
+        console.log("its work")
+        this.mapAnimation.step = 1;
+      } else if ( this.map.offsetStart + wrapperSection + 1 < currentHieght && currentHieght < this.map.offsetStart + wrapperSection * 2 ) {
+        this.mapAnimation.step = 2;
+      } else if ( this.map.offsetStart + wrapperSection * 2 + 1 < currentHieght && currentHieght < this.map.offsetStart + wrapperSection * 3 ) {
+        this.mapAnimation.step = 3;
+      } else if ( this.map.offsetStart + wrapperSection * 3 + 1 < currentHieght && currentHieght < this.map.offsetStart + wrapperSection * 4 ) {
+        this.mapAnimation.step = 4;
+      } else if ( this.map.offsetStart + wrapperSection * 4 + 1 < currentHieght && currentHieght < this.map.offsetStart + wrapperSection * 5 ) {
+        this.mapAnimation.step = 5;
+      } else if ( this.map.offsetStart + wrapperSection * 5 + 1 < currentHieght && currentHieght < this.map.offsetStart + wrapperSection * 6 ) {
+        this.mapAnimation.step = 6;
       } else {
-        this.step = 0;
+        this.mapAnimation.step = 0;
       }
-      // if (this.map.offset < currentHieght && currentHieght < this.map.offset + 1000 ) {
-      //   this.step = 1;
-      // } else if ( this.map.offset + 1001 < currentHieght && currentHieght < this.map.offset + 2000 ) {
-      //   this.step = 2;
-      // } else if ( this.map.offset + 2001 < currentHieght && currentHieght < this.map.offset + 3000 ) {
-      //   this.step = 3;
-      // } else if ( this.map.offset + 3001 < currentHieght && currentHieght < this.map.offset + 4000 ) {
-      //   this.step = 4;
-      // } else if ( this.map.offset + 4001 < currentHieght && currentHieght < this.map.offset + 5000 ) {
-      //   this.step = 5;
-      // } else {
-      //   this.step = 0;
-      // }
     }
   },
   mounted () {
-    // let bodyRect   = document.body.getBoundingClientRect(),
-    //     mapStartElement = document.querySelector('.page-start'),  
-    //     mapStartRect   = mapStartElement.getBoundingClientRect(),
-    //     mapEndElement = document.querySelector('.page-end'),  
-    //     mapEndRect   = mapEndElement.getBoundingClientRect(),
-    //     offsetStart     = mapStartRect.top - bodyRect.top,
-    //     offsetEnd       = mapEndRect.top - bodyRect.top ;
-
-    //     this.map.start = offsetStart;
-    //     this.map.end = offsetEnd;
-
-
-    //     console.log(offsetStart);
-    //     console.log(offsetEnd);
     let bodyRect   = document.body.getBoundingClientRect(),
-        mapStartElement = document.querySelector('.start-map'),  
+        mapStartElement = document.querySelector('.hints-wrapper'),  
         mapStartRect  = mapStartElement.getBoundingClientRect(),
-        offsetStart   = mapStartRect.top - bodyRect.top,
-        mapEndElement = document.querySelector('.end-map'),  
-        mapEndRect    = mapEndElement.getBoundingClientRect(),
-        offsetEnd     = mapEndRect.top - bodyRect.top
-        // wrapper       = document.querySelector('.hints-wrapper')
-        // wrapper.style.height = document.body.clientHeight * 5
+        mapHeight = mapStartRect.height,
+        offsetStart   = mapStartRect.top - bodyRect.top
 
-        this.screenHeight = document.body.clientHeight
+        this.map.height = mapHeight
         this.map.offsetStart = offsetStart
-        this.map.offsetEnd = offsetEnd
 
-        console.log(this.screenHeight)
-        console.log(this.map.offsetStart)
-        console.log(this.map.offsetEnd)
-
+        console.log(mapHeight)
         window.addEventListener('scroll', this.handleScroll)
 
   },
@@ -169,7 +152,10 @@ h1 {
   cursor: pointer;
 }
 .start-map {
-  position: static;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
 }
 .hints-wrapper {
   @media only screen and (max-width: 600px) {
@@ -177,99 +163,103 @@ h1 {
     right: 0;
     height: 6000px;
   }
+  transition: all 0.5s;
+  background: url('../assets/map.jpg') no-repeat;
+  background-position: center 0;
   position: relative;
   height: 8000px;
 }
-.hints-wrapper > .hint-box1 {
-  @media only screen and (max-width: 600px) {
-    width: 100%;
-    right: 10;
-  }
-  position: absolute;
-  z-index: 2;
-  right: 100px;
-  top: 0%;
-  height: 200px;
-  width: 400px;
-  border: solid 1px	#00BFFF;
-  border-radius: 5px;
-  background-color: rgba($color: white, $alpha: 0.2)
-}
-.hints-wrapper > .hint-box2 {
-  @media only screen and (max-width: 600px) {
-    width: 100vw;
-    right: 0;
-  }
-  position: absolute;
-  z-index: 2;
-  right: 100px;
-  top: 20%;
-  height: 200px;
-  width: 400px;
-  border: solid 1px	#00BFFF;
-  border-radius: 5px;
-  background-color: rgba($color: white, $alpha: 0.2)
-}
-.hints-wrapper > .hint-box3 {
-  @media only screen and (max-width: 600px) {
-    width: 100vw;
-    right: 0;
-  }
-  position: absolute;
-  z-index: 2;
-  right: 100px;
-  top: 40%;
-  height: 200px;
-  width: 400px;
-  border: solid 1px	#00BFFF;
-  border-radius: 5px;
-  background-color: rgba($color: white, $alpha: 0.2)
-}
-.hints-wrapper > .hint-box4 {
-  @media only screen and (max-width: 600px) {
-    width: 100vw;
-    right: 0;
-  }
-  position: absolute;
-  z-index: 2;
-  right: 100px;
-  top: 60%;
-  height: 200px;
-  width: 400px;
-  border: solid 1px	#00BFFF;
-  border-radius: 5px;
-  background-color: rgba($color: white, $alpha: 0.2)
-}
-.hints-wrapper > .hint-box5 {
-  @media only screen and (max-width: 600px) {
-    width: 100vw;
-    right: 0;
-  }
-  position: absolute;
-  z-index: 2;
-  right: 100px;
-  top: 80%;
-  height: 200px;
-  width: 400px;
-  border: solid 1px	#00BFFF;
-  border-radius: 5px;
-  background-color: rgba($color: white, $alpha: 0.2)
-}
-.end-map {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-}
-.map-fixed {
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  opacity: 1;
-  transform: scale(1);
-  transform-origin: 0 0;
-  height: auto;
-  transition: all 0.5s;
-}
+// .hints-wrapper > .hint-box1 {
+//   @media only screen and (max-width: 600px) {
+//     width: 100%;
+//     right: 10;
+//   }
+//   position: absolute;
+//   z-index: 2;
+//   right: 100px;
+//   top: 2.5%;
+//   height: 200px;
+//   width: 400px;
+//   border: solid 1px	#00BFFF;
+//   border-radius: 5px;
+//   background-color: rgba($color: white, $alpha: 0.2)
+// }
+// .hints-wrapper > .hint-box2 {
+//   @media only screen and (max-width: 600px) {
+//     width: 100vw;
+//     right: 0;
+//   }
+//   position: absolute;
+//   z-index: 2;
+//   right: 100px;
+//   top: 22.5%;
+//   height: 200px;
+//   width: 400px;
+//   border: solid 1px	#00BFFF;
+//   border-radius: 5px;
+//   background-color: rgba($color: white, $alpha: 0.2)
+// }
+// .hints-wrapper > .hint-box3 {
+//   @media only screen and (max-width: 600px) {
+//     width: 100vw;
+//     right: 0;
+//   }
+//   position: absolute;
+//   z-index: 2;
+//   right: 100px;
+//   top: 42.5%;
+//   height: 200px;
+//   width: 400px;
+//   border: solid 1px	#00BFFF;
+//   border-radius: 5px;
+//   background-color: rgba($color: white, $alpha: 0.2)
+// }
+// .hints-wrapper > .hint-box4 {
+//   @media only screen and (max-width: 600px) {
+//     width: 100vw;
+//     right: 0;
+//   }
+//   position: absolute;
+//   z-index: 2;
+//   right: 100px;
+//   top: 62.5%;
+//   height: 200px;
+//   width: 400px;
+//   border: solid 1px	#00BFFF;
+//   border-radius: 5px;
+//   background-color: rgba($color: white, $alpha: 0.2)
+// }
+// .hints-wrapper > .hint-box5 {
+//   @media only screen and (max-width: 600px) {
+//     width: 100vw;
+//     right: 0;
+//   }
+//   position: absolute;
+//   z-index: 2;
+//   right: 100px;
+//   top: 82.5%;
+//   height: 200px;
+//   width: 400px;
+//   border: solid 1px	#00BFFF;
+//   border-radius: 5px;
+//   background-color: rgba($color: white, $alpha: 0.2)
+// }
+// .end-map {
+//   position: absolute;
+//   bottom: 0;
+//   left: 50%;
+//   transform: translateX(-50%);
+// }
+// .map-fixed {
+//   position: fixed;
+//   z-index: 1;
+//   top: 0;
+//   left: 0;
+//   opacity: 1;
+//   transform: scale(1);
+//   transform-origin: 0 0;
+//   height: auto;
+//   transition: all 0.5s;
+// }
 
 </style>
